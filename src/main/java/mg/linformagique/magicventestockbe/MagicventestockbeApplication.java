e9c4ffefb9c54bb6e9dea364144ab30c023969e7
Sinel Vinesh <vinesh.sinel@gmail.com>
2024-06-10 18:21:55 +0300

    Init project

---
+++ src/main/java/mg/linformagique/magicventestockbe/MagicventestockbeApplication.java
@@ -0,0 +1,13 @@
+package mg.linformagique.magicventestockbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicventestockbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicventestockbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/linformagique/magicventestockbe/MagicventestockbeApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.linformagique.magicventestockbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicventestockbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
